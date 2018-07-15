a22326fcc8687a6f30fb02ea8eac679ab1ffc9e4
RhPlus <x1wins@naver.com>
2018-07-16 02:13:38 +0900

    add has_many

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,4 +1,10 @@
 class Home < ApplicationRecord
   belongs_to :user
   belongs_to :layout
+  has_many :abouts
+  has_many :experience
+  has_many :educations
+  has_many :skills
+  has_many :ineterest
+  has_many :awards
 end
